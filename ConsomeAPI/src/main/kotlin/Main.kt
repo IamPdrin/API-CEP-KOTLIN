19a635d86792e9acecf435c10877ffdd3485bd1f
Pedro Wolski <pedrobwolski@gmail.com>
2024-08-11 15:53:18 -0300

    consumoAPI Kotlin

---
+++ ConsomeAPI/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+import com.google.gson.Gson
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val ler = Scanner(System.`in`)
+    val consumo = Consumo()
+    val gson = Gson()
+
+    print("Digite o cep que deseja buscar: ")
+    val cep = ler.nextInt()
+
+    val json = consumo.buscaEndereco(cep)
+    println(json)
+
+    val endereco = gson.fromJson(json, Endereco::class.java)
+    println(endereco.rua)
+}
